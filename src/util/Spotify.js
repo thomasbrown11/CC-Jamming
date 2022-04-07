246da5c3fa4af37d8241a73a24484a861b62b904
thomasbrown11 <93149238+thomasbrown11@users.noreply.github.com>
2022-04-07 15:03:41 -0400

    getAccessToken done

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,10 +1,14 @@
-const accessToken;
+const accessToken = '';
 
 const Spotify = {
   getAccessToken() {
     if (accessToken) {
       return accessToken
-    } else { }
+    }
+    //check for access token match 
+    const accessToken = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/)
   }
+}
 
 export default Spotify;
