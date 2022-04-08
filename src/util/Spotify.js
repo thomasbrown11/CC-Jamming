d2ccfb1af39d709d00c7ecd597798d7e7b3306d8
thomasbrown11 <93149238+thomasbrown11@users.noreply.github.com>
2022-04-08 08:33:48 -0400

    new logic for getAccessToken in Spotify module

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -6,8 +6,22 @@ const Spotify = {
       return accessToken
     }
     //check for access token match 
-    const accessToken = window.location.href.match(/access_token=([^&]*)/);
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
     const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/)
+
+    //if accessToken and expiration exists in current URL 
+    if (accessTokenMatch && expiresInMatch) {
+      //set accessToken to URL match
+      accessToken = accessTokenMatch[1];
+      //set variable for expiration equal to a number taken from URL string 
+      const expiresIn = Number(expiresInMatch[1]);
+      //set access token expiration
+      window.setTimeout(() => { accessToken = '', expiresIn * 1000 });
+      //clear URL parameters so that a new accessToken can be grabbed upon expiration 
+      window.history.pushState('Access Token', null, '/');
+      return accessToken;
+
+    }
   }
 }
 
