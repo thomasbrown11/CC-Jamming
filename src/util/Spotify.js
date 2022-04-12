4e98ee2e88003e21819184a8590c6997529ae441
thomasbrown11 <93149238+thomasbrown11@users.noreply.github.com>
2022-04-12 08:32:31 -0400

    debugged POST request in savePlaylist, added description comments

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -33,13 +33,19 @@ const Spotify = {
   },
 
   search(searchTerm) {
+    //call getAccessToken for valid token, new variable here to use within this method specifically
     const accessToken = Spotify.getAccessToken;
+    //proper HTTP request detailed by Spotify leveraging fetch promise. 
+    //headers is an optional custom add-on which provides authorization
+    //this is the actual search query. 
     return fetch(`https://api.spotify.com/v1/search?type=track&q=${searchTerm}`, {
       headers: {
         Authorization: `Bearer ${accessToken}`
       }
+      //grab response from the GET request and convert to JSON for usability. 
     }).then(response => {
       return response.json();
+      //grab the new JSON object. If track isn't present return empty array, otherwise map a new properly formatted object
     }).then(jsonResponse => {
       if (!jsonResponse.track) {
         return [];
@@ -57,16 +63,34 @@ const Spotify = {
   },
 
   savePlaylist(playlist, uriArray) {
+    //failsafe, require these parameters are entered
     if (!playlist || uriArray) {
       return;
     }
+    //leverage above method to get valid token
     const accessToken = Spotify.getAccessToken();
+    //set header properly as we'll be using this on all HTTP request to validate
     const header = { Authorization: `Bearer ${accessToken}` }
+    //to be filled in by first HTTP (GET) Request's response
     let userID;
-
+    //return a GET request (endpoint provided by spotify), validate with header
+    //this specific request is to get a userID to work in a specific account
     return fetch('https://api.spotify.com/v1/me', { headers: header }
+      //format reponse to JSON object
     ).then(response => response.json()
-    ).then(jsonResponse => userID = jsonResponse.id);
+      //set userID to parameter in json object
+    ).then(jsonResponse => {
+      userID = jsonResponse.id;
+      //within this return make POST request. This specific endpoint maps to requesting a new playlist be created in user account
+      //the body parameter within fetch argument object will name the new 
+      //playlist initiated by POST whatever is passed in to savePlaylist().
+      return fetch(`https://api.spotify.com/v1/users/${userID}/playlists`, {
+        headers: header,
+        method: 'POST',
+        //must be converted to proper jason string to be read by request 
+        body: JSON.stringify({ name: playlist })
+      })
+    });
   }
 }
 
